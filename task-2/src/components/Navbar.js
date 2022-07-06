12429bca43539ce43e5f5ea2011d418f87ba809a
Mridul07Sharma <mridul07sharma@gmail.com>
2022-07-06 16:48:54 +0530

    Changed files

--- task-2/src/components/Navbar.js
+++ task-2/src/components/Navbar.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import '../css/navbar.css'
 export default function Navbar() {
+
+const [users,setUsers]=useState([]);
+const loadUsers=async ()=>{
+const response=await fetch("https://reqres.in/api/users?page=1");
+const jsonResponse=await response.json();
+setUsers(jsonResponse.data);
+}
+
   return (
     <>
-    <div className="hello">Hello World!!!</div></>
+    <button onClick={loadUsers}>Get Data</button>
+    <h2>Users:</h2>
+    <ul>
+{users.map(({id,first_name,last_name})=>(
+  <li key={id}>Name: {first_name} {last_name}</li>
+))}
+    </ul>
+    </>
   )
 }
