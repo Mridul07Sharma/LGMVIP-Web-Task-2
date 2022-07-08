e1049460a0e4e20924f05642bb4be3bd055fbfc8
Mridul07Sharma <mridul07sharma@gmail.com>
2022-07-08 20:08:36 +0530

    changed to axios

--- task-2/src/components/Display.js
+++ task-2/src/components/Display.js
@@ -1,40 +1,40 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/display.css'
+import axios from 'axios';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import { Bars } from  'react-loader-spinner';
-
+import { Bars } from 'react-loader-spinner';
 export default function Display() {
-  const [users,setUsers]=useState(null);
+  const [users, setUsers] = useState(null);
+
   useEffect(() => {
-    const loadUsers=async ()=>{
-      const response=await fetch("https://reqres.in/api/users?page=1");
-      const jsonResponse=await response.json();
-      setUsers(jsonResponse.data);
-      }
-      loadUsers()
+    axios.get('https://reqres.in/api/users?page=1')
+      .then((res) => {
+        setUsers(res.data);
+      })
   }, [])
-if(users){
-    return(
+
+  if (users) {
+    return (
       <>
-  <div className="displayyy">      
-  {users.map(({id,first_name,last_name,email,avatar})=>(
-  <div className="card setr" style={{width:' 18rem'}} key={id}>
-    <img src={avatar} className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title text-center lettt">{first_name} {last_name}</h5>
-      <p className="card-text text-center met"><b>E-mail:</b> {email}</p>
-   </div>
-  </div>
-  ))}
-  </div>  
-  </>
+        <div className="displayyy">
+          {users.data.map(({ id, first_name, last_name, email, avatar }) => (
+            <div className="card setr" style={{ width: ' 18rem' }} key={id}>
+              <img src={avatar} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title text-center lettt">{first_name} {last_name}</h5>
+                <p className="card-text text-center met"><b>E-mail:</b> {email}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
     )
   }
-  return(
-    <><div className="loader">  
-    <Bars color="#377D71" height={80} width={80} />
+  return (
+    <><div className="loader">
+      <Bars color="#377D71" height={80} width={80} />
     </div>
     </>
   )
-  
+
 }
--- task-2/src/components/Navbar.js
+++ task-2/src/components/Navbar.js
@@ -3,15 +3,15 @@ import '../css/navbar.css'
 import Display from "./Display";
 
 export default function Navbar() {
-const [bool, setBool] = useState(false);
+const [mode, setMode] = useState(false);
   return (
    <>
    <div className="nav">
     <div className="heading">LGMVIP </div>
-    <button type='button' className='sett' onClick = {() => setBool(true) } >Get Users</button>
+    <button type='button' className='sett' onClick = {() => setMode(true) } >Get Users</button>
     </div>
     <main>
-    { bool ? <Display /> : <div className="demp">  
+    { mode ? <Display /> : <div className="demp">  
         <p> Click on Get 'Users Button' to get the data from API.</p></div>}
      </main>
     </>
